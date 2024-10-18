c6989335f97f529ecc3ec30ea7e9484910224924
rahul97701 <111692381+rahul97701@users.noreply.github.com>
2024-10-18 15:26:53 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./App.css";
+import Header from "./app/components/Header";
+import Home from "./app/components/Home";
+import Detail from "./app/components/Detail";
+import Login from "./app/components/Login";
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Routes>
+          <Route path="/detail/:id" element={<Detail />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+      </Routes>
+   </div>
+  );
+}
+
+export default App;
